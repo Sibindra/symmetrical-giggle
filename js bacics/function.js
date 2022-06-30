160443bb66025db2c4e8faff84984f92cadbe42f
Sibindra <timalsinasibindra@gmail.com>
2022-06-30 09:18:46 +0545

    js day 1

---
+++ js bacics/function.js
@@ -0,0 +1,8 @@
+function display(nam){
+    console.log(nam);
+}
+
+let b=10;
+console.log(`the name is ${b}`)
+
+display("suresh");
---
+++ js bacics/variables.js
@@ -0,0 +1,10 @@
+const abc="Jhon";
+
+console.log(abc);
+
+let a=[
+    {"name":"jhon","address":"kathmandu"},
+    {"name":"amar","address":"bhaktapur"},
+];
+
+console.log(a);
