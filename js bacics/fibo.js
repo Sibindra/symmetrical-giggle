85faf9779c7b5418381c6f34f3527cba60e1c3fb
Sibindra <timalsinasibindra@gmail.com>
2022-07-01 09:29:58 +0545

    friday

---
+++ js bacics/fibo.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function fibo(n){
+    if(n<2){
+        return n;
+    }else{
+        return (fibo(n-1)+fibo(n-2));
+    }
+}
+
+for(let i=0;i<20;i++){
+    console.log(fibo(i));
+}
--- js bacics/function.js
+++ js bacics/function.js
@@ -6,3 +6,4 @@ let b=10;
 console.log(`the name is ${b}`)
 
 display("suresh");
+
